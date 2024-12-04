8c64f77eca7785959aa52fa49b2576a10f768a47
Nathan-Vocino <144161279+Nathan-Vocino@users.noreply.github.com>
2024-12-03 22:27:58 -0500

    Update Organize_event.php

--- App/Organize_event.php
+++ App/Organize_event.php
@@ -1,3 +1,9 @@
+<?php
+if (!isset($_SESSION['member_id'])) {
+    echo "You must be logged in to view this page.";
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <link rel="stylesheet" type = "text/css" href="../css/index.css" />
