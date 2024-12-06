e261881de29e10802d7eaffc9748f944ea4bdae1
Nathan-Vocino <144161279+Nathan-Vocino@users.noreply.github.com>
2024-12-05 22:12:44 -0500

    Update chatbox.php

--- App/chatbox.php
+++ App/chatbox.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['member_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chatbox</title>
-    <link rel="stylesheet" type="text/css" href="./css/index.css">
+    <link rel="stylesheet" type="text/css" href="./css/chatbox.css">
 </head>
 <body>
     <h1>Chatbox</h1>
