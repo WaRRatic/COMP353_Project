58f0c27d0c4227f1e717d931bd47c31647741953
Nathan-Vocino <144161279+Nathan-Vocino@users.noreply.github.com>
2024-12-07 13:53:53 -0500

    Update unfriend_user.php

--- App/unfriend_user.php
+++ App/unfriend_user.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include 'db.php';
+include 'db_config.php';
 
 if (!isset($_SESSION['member_id'])) {
     echo json_encode(['status' => 'error', 'message' => 'User not logged in']);
